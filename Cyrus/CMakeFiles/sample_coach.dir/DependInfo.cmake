f8fb88a3195f9356dbab34743fa3333901ae0e5c
HenriqueFrisso <henriquefifes@gmail.com>
2025-09-16 18:28:06 -0300

    Adicionado Cyrus

---
+++ Cyrus/CMakeFiles/sample_coach.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/titas2d/Henrique/cyrus-soccer-simulation-team/src/main_coach.cpp" "src/CMakeFiles/sample_coach.dir/main_coach.cpp.o" "gcc" "src/CMakeFiles/sample_coach.dir/main_coach.cpp.o.d"
+  "/home/titas2d/Henrique/cyrus-soccer-simulation-team/src/sample_coach.cpp" "src/CMakeFiles/sample_coach.dir/sample_coach.cpp.o" "gcc" "src/CMakeFiles/sample_coach.dir/sample_coach.cpp.o.d"
+  "/home/titas2d/Henrique/cyrus-soccer-simulation-team/src/sample_freeform_message.cpp" "src/CMakeFiles/sample_coach.dir/sample_freeform_message.cpp.o" "gcc" "src/CMakeFiles/sample_coach.dir/sample_freeform_message.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Cyrus/CMakeFiles/sample_player.dir/chain_action/DataExtractor.cpp.o.d
@@ -0,0 +1,2 @@
+src/CMakeFiles/sample_player.dir/chain_action/DataExtractor.cpp.o: \
+  /home/titas2d/Henrique/cyrus-soccer-simulation-team/src/chain_action/DataExtractor.cpp
